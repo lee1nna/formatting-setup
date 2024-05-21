36f7df3e81fcca32e24d9d6a83e8354e9499f0e5
lee1nna <dlgkssk0209@gmail.com>
2024-05-21 22:58:24 +0900

    Chore: fix eslint prettier crush

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,40 @@
 module.exports = {
-  root: true,
-  plugins: ['import', 'react', '@typescript-eslint'],
-  extends: [
-    'plugin:import/recommended',
-    'plugin:react/recommended',
-    'prettier',
-    'prettier/prettier',
-    'plugin:prettier/recommended',
-    'airbnb',
-  ],
-  rules: {
-    'no-console': 'error',
-    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        ts: 'never',
-        tsx: 'never',
-      },
+    root: true,
+    plugins: ['import', 'react', '@typescript-eslint'],
+    extends: [
+        'plugin:import/recommended',
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:prettier/recommended',
     ],
-  },
-  env: {
-    browser: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
+    rules: {
+        'no-console': 'error',
+        'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
     },
-    ecmaVersion: 11,
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {},
+    env: {
+        browser: true,
+        node: true,
     },
-  },
-};
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 11,
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {},
+        },
+    },
+}
